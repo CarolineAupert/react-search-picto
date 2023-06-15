75ea28e3140ca5a38aba6d069db4178eb24c9c12
Caroline Aupert <caroline.aupert@gmail.com>
2023-06-15 17:26:01 +0200

    Add SEO metadata

--- src/components/search/AboutZone.test.js
+++ src/components/search/AboutZone.test.js
@@ -2,14 +2,17 @@ import { render, screen } from '@testing-library/react';
 import { React } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import AboutZone from './AboutZone';
+import { HelmetProvider } from 'react-helmet-async';
 
 describe('AboutZone', () => {
     it("should display the about page", async () => {
 
         render(
-            <MemoryRouter initialEntries={["/about"]}>
-                <AboutZone/>
-            </MemoryRouter >
+            <HelmetProvider>
+                <MemoryRouter initialEntries={["/about"]}>
+                    <AboutZone />
+                </MemoryRouter >
+            </HelmetProvider>
         );
         expect(screen.getByTestId("about-author")).toBeInTheDocument();
         expect(screen.getByTestId("about-faq")).toBeInTheDocument();
